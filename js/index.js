12724114871236c5b07b8986384f92f9c1b7fe59
Lutz Dietterich <lutzdietterich@googlemail.com>
2023-02-01 19:38:27 +0100

    addDarkButton

--- js/index.js
+++ js/index.js
@@ -5,3 +5,9 @@ const bodyElement = document.querySelector('[data-js="body"]');
 const darkModeButton = document.querySelector('[data-js="dark-mode-button"]');
 const lightModeButton = document.querySelector('[data-js="light-mode-button"]');
 const toggleButton = document.querySelector('[data-js="toggle-button"]');
+
+darkModeButton.addEventListener("click", () => {
+  console.log(darkModeButton);
+  console.log(bodyElement);
+  bodyElement.classList.add("button--dark");
+});
